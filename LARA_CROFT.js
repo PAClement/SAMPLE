668360bb14dd8257814d969948a94a063e4be999
clement <clementpaquentin@hotmail.fr>
2023-04-02 18:23:56 +0200

    FIX & TEST

--- LARA_CROFT.js
+++ LARA_CROFT.js
@@ -1,4 +1,5 @@
-const dessins = ['0:A 6:V 7:V 8:L', '0:J 1:A 6:V 8:V', '0:A 1:A 2:A 6:J 7:V', '0:A 2:L 3:J 6:V 7:V', '0:A 2:A 6:V 7:V', '0:A 3:J 5:L 6:V', '2:L 3:J 6:V', '0:A 5:L 7:V', '1:A 5:L 6:J 8:V', '0:A 2:A 3:J 5:L 6:J 8:L', '2:L 5:L 6:J 8:L', '3:J 7:V 8:L', '0:A 2:A 3:J 5:L 6:J 8:V', '0:J 1:A 2:L 3:J 5:L 6:J', '0:J 6:J 8:L'];let listGrille = []
+const dessins = ['1:A 2:L 6:J 7:V 8:V', '0:J 3:J 5:L 6:V 7:V', '1:A 2:A 5:L 6:J 8:L', '0:A 2:L 3:J 6:V 8:V', '1:A 2:A 5:L 6:J 7:V 8:V', '0:A 1:A 3:J 5:L 6:V 8:V', '0:J 3:J 5:L 6:V 8:L', '1:A 2:L 5:L 6:J 8:L', '0:J 2:L 3:J 5:L 7:V 8:L', '1:A 6:J', '0:A 1:A 2:A 3:J 5:L 6:V 7:V', '2:A 6:V 7:V 8:L', '0:A 1:A 2:L 3:J 7:V 8:V', '0:A 2:A 3:J 6:J 7:V', '0:J 1:A 2:A 5:L 6:J 8:V'];
+let listGrille = []
 let result = ''
 
 function nullable(array) {
@@ -68,6 +69,5 @@ listGrille.forEach(target => {
         }
     }
 })
-console.log(listGrille)
 console.log(result)
 
